0b89ebc2e17b8b02f0fa787e26c47d629677479e
Brandon Rhodes <brandon@rhodesmill.org>
2020-09-22 16:47:02 -0400

    Remind myself where to get Docker environment

--- run.sh
+++ run.sh
@@ -2,11 +2,17 @@
 
 set -e
 
+if ! [ -d ~/docker-connectiq ]
+then
+    echo 'In your home directory, run:'
+    echo 'git clone https://github.com/brandon-rhodes/docker-connectiq.git'
+    exit 1
+fi
+
 BASE="$(readlink -f $(dirname "${BASH_SOURCE[0]}"))"
 echo $BASE
 
 export CIQ_WORKSPACE="$BASE"
-#export COMMAND="ls /home/developer/eclipse-workspace"
 export COMMAND="/bin/bash /home/developer/eclipse-workspace/monkey.sh"
 
 cd ~/docker-connectiq
